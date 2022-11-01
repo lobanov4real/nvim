fc9685d72864f5fca74dbca55756649800a97943
lobanov4real <lobanov4real@gmail.com>
2022-11-01 22:11:51 +0300

    add some functionality

--- lua/ivan/core/keymaps.lua
+++ lua/ivan/core/keymaps.lua
@@ -1,3 +1,29 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+
+-- general keymap
+
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "x", '"_x"')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+keymap.set("n", "<leader>sv", "<C-w>v")  -- split window vertivaly
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontaly
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabn<CR>") -- go to previous tab
+
+-- plugin keymaps
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
