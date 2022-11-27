48f11e0933832fb9f4584ee46c035460fadd1c74
lobanov4real <lobanov4real@gmail.com>
2022-11-27 00:26:21 -0800

    edit plugin-setup

--- lua/ivan/plugins-setup.lua
+++ lua/ivan/plugins-setup.lua
@@ -61,7 +61,7 @@ return packer.startup(function(use)
 	use("hrsh7th/cmp-path")
 
 	-- snippets
-	use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
+	use({ "L3MON4D3/LuaSnip", tag = "v1.*" })
 	use("saadparwaiz1/cmp_luasnip")
 	use("rafamadriz/friendly-snippets")
 
