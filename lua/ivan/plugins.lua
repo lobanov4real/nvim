6f0b8b40a5cee1933aaf770ebf57bd9f3997a2cf
lobanov4real <lobanov4real@gmail.com>
2022-12-15 17:52:32 +0300

    add some plugins

--- lua/ivan/plugins.lua
+++ lua/ivan/plugins.lua
@@ -31,7 +31,8 @@ packer.startup(function(use)
       ts_update()
     end,
   }
-
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions and more via Lua
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
 
