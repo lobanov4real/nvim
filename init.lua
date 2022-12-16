9a8f46177967fba8e145542773aa914b4fb67dc5
lobanov4real <lobanov4real@gmail.com>
2022-12-16 23:12:05 +0300

    refactoring

--- init.lua
+++ init.lua
@@ -4,9 +4,9 @@ require('ivan.maps')
 require('ivan.plugins')
 
 local has = vim.fn.has
+
 local is_mac = has "macunix"
 local is_win = has "win32"
-
 if is_mac then
   require('ivan.macos')
 end
--- lua/ivan/base.lua
+++ lua/ivan/base.lua
@@ -32,10 +32,6 @@ vim.opt.wrap = false -- No Wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = '*',
