01b1b0ba2703ac01fe667ed68f575c688e4ad7fe
lobanov4real <lobanov4real@gmail.com>
2022-12-17 18:34:07 +0300

    add some plugins and configs

--- lua/ivan/plugins.lua
+++ lua/ivan/plugins.lua
@@ -15,20 +15,36 @@ packer.startup(function(use)
   use 'nvim-lualine/lualine.nvim' -- Statusline
   use 'nvim-lua/plenary.nvim' -- Common utilities
   use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'onsails/lspkind-nvim' -- VSCode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- Nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- Nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use 'jose-elias-alvarez/typescript.nvim'
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
+
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
+
   use 'Yggdroot/indentLine'
-  use 'mtdl9/vim-log-highlighting'
-  use 'numToStr/Comment.nvim'
-  use { 'akinsho/nvim-bufferline.lua', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+
+  use 'mtdl9/vim-log-highlighting' -- Log files colorizing
+
+  use 'numToStr/Comment.nvim' -- Comment with gcc
+
+  use { 'akinsho/nvim-bufferline.lua', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' } -- Tabs for Nvim
+
   use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
   use 'norcalli/nvim-colorizer.lua'
 end)
