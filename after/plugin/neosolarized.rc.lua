4a583922856a2eb3bfc970a26ca82b197c16c446
lobanov4real <lobanov4real@gmail.com>
2023-07-28 09:23:40 +0300

    refactoring

--- after/plugin/neosolarized.rc.lua
+++ after/plugin/neosolarized.rc.lua
@@ -15,7 +15,7 @@ local styles = cb.styles
 Color.new('white', '#ffffff')
 Color.new('black', '#000000')
 Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
-Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+Group.new('CursorLine', colors.base1, colors.base1, styles.NONE, colors.base1)
 Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
 Group.new('Visual', colors.none, colors.base03, styles.reverse)
 
