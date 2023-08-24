4a0964c63c0050a9c21daa2139d13e25247df407
lobanov4real <lobanov4real@gmail.com>
2023-08-24 17:29:02 +0300

    refactoring

--- lua/ivan/highlights.lua
+++ lua/ivan/highlights.lua
@@ -3,7 +3,7 @@ vim.opt.termguicolors = true
 vim.opt.winblend = 0
 vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
-vim.opt.background = 'light'
+vim.opt.background = 'dark'
 
 
 -- highlight yanked text for 200ms using the "Visual" highlight group
