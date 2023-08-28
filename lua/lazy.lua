a498bed5bde9a06a74b1616870ffdb46c38aa6d2
lobanov4real <lobanov4real@gmail.com>
2023-08-28 14:37:07 +0300

    rebuild

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -10,4 +10,16 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins, opts)
+
+require("lazy").setup({ { import = "plugins" } }, {
+  install = {
+    colorscheme = { "nightfly" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
