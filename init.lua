1aa5ce48aa91085eaba485f5aebead2d21ee27db
lobanov4real <lobanov4real@gmail.com>
2023-08-29 16:34:41 +0300

    refactoring

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.highlights")
-require("config.lazy")
 require("config.options")
 require("config.maps")
+require("config.lazy")
 
 local has = vim.fn.has
 local is_mac = has("macunix")
